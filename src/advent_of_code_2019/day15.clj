b319c1b690506d38033387d9c8f479ed7b2b65c0
FiVo <finn.volkel@gmail.com>
2019-12-15 23:47:04 +0100

    Day 15.

--- src/advent_of_code_2019/day15.clj
+++ src/advent_of_code_2019/day15.clj
@@ -39,7 +39,7 @@
                 front
                 (conj front [(assoc-expand input (get input (inc i)) j m1 r)
                              (+ i 2) r next]))))
-          [] (range 2 5)))
+          [] (range 1 5)))
 
 (defn concat-vec [& vs]
   (vec (apply concat vs)))
@@ -48,7 +48,6 @@
   ([input] (run-program input 0 0 [[0 0] 0] #{[0 0]} []))
   ([input i r cur seen front]
    (loop [input input i i r r cur cur seen seen front front]
-     ;; (println i " " (get input i))
      (match (int-to-array (get input i))
             [m3 m2 m1 1] (recur (assoc-expand input (get input (+ i 3))
                                               (+' (get-program-value2 input m1 (inc i) r)
@@ -60,10 +59,14 @@
                                                   (get-program-value2 input m2 (+ i 2) r))
                                               m3 r)
                                 (+ i 4) r cur seen front)
-            [_ _ m1 3] (let [newinput (assoc-expand input (get input (inc i)) 1 m1 r)
-                             next (add-vectors cur (first steps))
-                             newfront (calculate-new-front input i r m1 cur seen)]
-                         (recur newinput (+ i 2) r next seen (concat-vec front newfront)))
+            [_ _ m1 3] (let [newfront (calculate-new-front input i r m1 cur seen)]
+                         (cond
+                           (empty? newfront)
+                           (let [[newinput ii rr next] (first front)]
+                                (recur newinput ii rr next seen (subvec front 1)))
+                           :else
+                           (let [[newinput ii rr next] (first newfront)] 
+                            (recur newinput ii rr next seen (concat-vec front (subvec newfront 1))))))
             [_ _ m1 4]
             (if (empty? front)
               (case (get-program-value2 input m1 (inc i) r)
@@ -103,4 +106,100 @@
        vec
        run-program))
 
-(day15-a "resources/input15.txt")
+;; (day15-a "resources/input15.txt")
+(def coordinates (first *1))
+
+(defn add-vectors2 [[x1 y1] [x2 y2]]
+  [(+ x1 x2) (+ y1 y2)])
+
+(defn calculate-new-front2 [input i r m1 cur seen]
+  (reduce (fn [front j]
+            (let [next (add-vectors2 cur (nth steps (dec j)))]
+              (if (contains? seen next)
+                front
+                (conj front [(assoc-expand input (get input (inc i)) j m1 r)
+                             (+ i 2) r next]))))
+          [] (range 1 5)))
+
+(defn run-program2
+  ([input] (run-program2 input 0 0 [0 0] {[0 0] 1} []))
+  ([input i r cur seen front]
+   (loop [input input i i r r cur cur seen seen front front]
+     ;; (println i " " (get input i))
+     (match (int-to-array (get input i))
+            [m3 m2 m1 1] (recur (assoc-expand input (get input (+ i 3))
+                                              (+' (get-program-value2 input m1 (inc i) r)
+                                                  (get-program-value2 input m2 (+ i 2) r))
+                                              m3 r)
+                                (+ i 4) r cur seen front)
+            [m3 m2 m1 2] (recur (assoc-expand input (get input (+ i 3))
+                                              (*' (get-program-value2 input m1 (inc i) r)
+                                                  (get-program-value2 input m2 (+ i 2) r))
+                                              m3 r)
+                                (+ i 4) r cur seen front)
+            [_ _ m1 3] (let [newfront (calculate-new-front2 input i r m1 cur seen)]
+                         (cond
+                           (and (empty? newfront) (empty? front)) seen
+                           (empty? newfront)
+                           (let [[newinput ii rr next] (first front)]
+                             (recur newinput ii rr next seen (subvec front 1)))
+                           :else
+                           (let [[newinput ii rr next] (first newfront)] 
+                             (recur newinput ii rr next seen (concat-vec front (subvec newfront 1))))))
+            [_ _ m1 4]
+            (if (empty? front)
+              (case (get-program-value2 input m1 (inc i) r)
+                0 seen 
+                1 (recur input (+ i 2) r cur (assoc seen cur 1) [])
+                2 (recur input (+ i 2) r cur (assoc seen cur 1) []))
+              (let [[newinput ii rr next] (first front)]
+                (case (get-program-value2 input m1 (inc i) r)
+                  0 (recur newinput ii rr next (assoc seen cur 0) (subvec front 1)) 
+                  1 (recur newinput ii rr next (assoc seen cur 1) (conj (subvec front 1) [input (+ i 2) r cur]))
+                  2 (recur newinput ii rr next (assoc seen cur 1) (conj (subvec front 1) [input (+ i 2) r cur])))))
+            [_ m2 m1 5] (if (= (get-program-value2 input m1 (inc i) r) 0)
+                          (recur input (+ i 3) r cur seen front)
+                          (recur input (get-program-value2 input m2 (+ i 2) r) r cur seen front))
+            [_ m2 m1 6] (if (= (get-program-value2 input m1 (inc i) r) 0)
+                          (recur input (get-program-value2 input m2 (+ i 2) r) r cur seen front)
+                          (recur input (+ i 3) r cur seen front))
+            [m3 m2 m1 7] (->
+                          (assoc-expand input (get input (+ i 3))
+                                        (if (< (get-program-value2 input m1 (inc i) r)
+                                               (get-program-value2 input m2 (+ i 2) r)) 1 0)
+                                        m3 r)
+                          (recur (+ i 4) r cur seen front))
+            [m3 m2 m1 8] (->
+                          (assoc-expand input (get input (+ i 3))
+                                        (if (= (get-program-value2 input m1 (inc i) r)
+                                               (get-program-value2 input m2 (+ i 2) r)) 1 0)
+                                        m3 r)
+                          (recur (+ i 4) r cur seen front))
+            [_ _ m1 9]  (recur input (+ i 2) (+ r (get-program-value2 input m1 (inc i) r)) cur seen front)
+            [_ _ _ 99] input
+            :else (throw (Exception. "Should not happen!!!"))))))
+
+(defn bfs [[startx starty] seen]
+  (loop [res {[startx starty] 0} nexts [[startx starty]]]
+    (if (empty? nexts) res
+        (let [[x y] (first nexts)
+              dis (get res [x y])
+              newnexts (reduce (fn [v [i j]]
+                                 (cond
+                                   (contains? res [(+ x i) (+ y j)]) v
+                                   (= (get seen [(+ x i) (+ y j)]) 1) (conj v [(+ x i) (+ y j)])
+                                   :else v)) [] steps)
+              newres (reduce (fn [res cur]
+                               (assoc res cur (inc dis))) res newnexts)]
+          (recur newres (concat-vec (subvec nexts 1) newnexts))))))
+
+(defn day15-b [input]
+  (->> input
+       read-input2
+       vec
+       run-program2
+       (bfs coordinates)
+       vals
+       (apply max)))
+
+;; (day15-b "resources/input15.txt")
