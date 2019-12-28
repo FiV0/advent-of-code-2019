3858b7f7637ab3dbdd38c67d44756a9b7a7a7d02
FiVo <finn.volkel@gmail.com>
2019-12-28 11:15:00 +0100

    Day 20. Part b.

--- src/advent_of_code_2019/day20.clj
+++ src/advent_of_code_2019/day20.clj
@@ -85,5 +85,88 @@
         [start end portal-map] (build-portal-map m)]
     (bfs start end m portal-map)))
 
-(day20-a "resources/input20.txt")
-(day20-a "resources/test20.txt")
+;; (day20-a "resources/input20.txt")
+;; (day20-a "resources/test20.txt")
+
+(defn outer-pos? [[x y] max-x max-y]
+  (or (<= x 3) (<= y 3)
+      (>= x (- max-x 3)) (>= y (- max-y 3))))
+
+(defn add-outer-inner [outer inner pos1 pos2 max-x max-y]
+  (let [[outer inner]
+        (if (outer-pos? pos1 max-x max-y)
+          [(assoc outer pos1 pos2) inner]
+          [outer (assoc inner pos1 pos2)])]
+    (if (outer-pos? pos2 max-x max-y)
+      [(assoc outer pos2 pos1) inner]
+      [outer (assoc inner pos2 pos1)])))
+
+(defn build-portal-map2 [m]
+  (let [max-x (->> (keys m)
+                   (map first)
+                   (apply max))
+        max-y (->> (keys m)
+                   (map second)
+                   (apply max))
+        location-map (->> (for [x (range (inc max-x))
+                                y (range (inc max-y))]
+                            [x y])
+                          (map apply-two-step-nhood)
+                          (apply concat)
+                          (filter (fn [[pos1 pos2 pos3]]
+                                    (and (every? false? (map #(nil? (get m %)) [pos1 pos2 pos2]))
+                                         (Character/isUpperCase (get m pos1))
+                                         (Character/isUpperCase (get m pos2))
+                                         (= (get m pos3) \.))))
+                          (map (fn [[pos1 pos2 pos3]]
+                                 (let [[x y] (subtract-vectors pos2 pos3)]
+                                   (if (or (neg? x) (neg? y))
+                                     [(str (get m pos2) (get m pos1)) pos3]
+                                     [(str (get m pos1) (get m pos2)) pos3]))))
+                          (reduce (fn [res [s pos]]
+                                    (if (nil? (get res s))
+                                      (assoc res s [pos])
+                                      (update res s #(conj % pos)))) {}))
+        start (first (get location-map "AA"))
+        end (first (get location-map "ZZ"))
+        [outer-portal inner-portal] (->> (dissoc location-map "AA" "ZZ")
+                                         vals
+                                         (reduce (fn [[outer inner] [pos1 pos2]]
+                                                   (add-outer-inner outer inner pos1 pos2 max-x max-y))
+                                                 [{} {}]))]
+    [start end outer-portal inner-portal]))
+
+(defn add-neigbours2 [pos level dis m queue seen]
+  (reduce (fn [[nq ns] dir]
+            (let [next (add-vectors pos dir)]
+              (if (and (= (get m next) \.) (not (contains? ns [next level])))
+                [(conj nq [[next level] (inc dis)]) (conj ns [next level])]
+                [nq ns]))) [queue seen] nhood))
+
+(defn bfs2 [start end m outer inner]
+  (loop [queue (conj (clojure.lang.PersistentQueue/EMPTY) [[start 0] 0])
+         seen #{[start 0]}]
+    (assert (not (empty? queue)))
+    (let [[[cur level] dis] (peek queue)]
+      (if (= [cur level] [end 0])
+        dis
+        (let [[new-queue new-seen] (add-neigbours2 cur level dis m (pop queue) seen)]
+          (cond (and (not (nil? (get outer cur)))
+                     (> level 0)
+                     (not (contains? new-seen [(get outer cur) (dec level)])))
+                (recur (conj new-queue [[(get outer cur) (dec level)] (inc dis)])
+                       (conj new-seen [(get outer cur) (dec level)]))
+                (and (not (nil? (get inner cur)))
+                     (not (contains? new-seen [(get inner cur) (inc level)])))
+                (recur (conj new-queue [[(get inner cur) (inc level)] (inc dis)])
+                       (conj new-seen [(get inner cur) (inc level)]))
+                :else (recur new-queue new-seen)))))))
+
+(defn day20-b [file]
+  (let [m (read-input file)
+        [start end outer inner] (build-portal-map2 m)]
+    (bfs2 start end m outer inner)))
+
+;; (day20-b "resources/input20.txt")
+;; (day20-b "resources/test20.txt")
+;; (day20-b "resources/test20b.txt")
